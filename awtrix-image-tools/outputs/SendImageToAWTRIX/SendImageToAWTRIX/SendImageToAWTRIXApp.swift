947733ebec84f2d6f1ffb84e7b0687bc728acbeb
Duv McIntyre <duv@mudbarn.com>
2026-06-08 15:58:18 -0700

    Use UIKit app startup on iOS

--- awtrix-image-tools/outputs/SendImageToAWTRIX/SendImageToAWTRIX/SendImageToAWTRIXApp.swift
+++ awtrix-image-tools/outputs/SendImageToAWTRIX/SendImageToAWTRIX/SendImageToAWTRIXApp.swift
@@ -1,10 +1,18 @@
 import SwiftUI
+import UIKit
 
 @main
-struct SendImageToAWTRIXApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = UIHostingController(rootView: ContentView())
+        window.makeKeyAndVisible()
+        self.window = window
+        return true
     }
 }
