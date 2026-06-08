8fdfa4ddbf19ff5e2cde34aad7e0c347bafd3976
Duv McIntyre <duv@mudbarn.com>
2026-06-08 16:12:05 -0700

    Add explicit iPad scene lifecycle

---
+++ awtrix-image-tools/outputs/SendImageToAWTRIX/SendImageToAWTRIX/SceneDelegate.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = LauncherViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+}
